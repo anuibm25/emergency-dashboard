49af0bee529e3a287b79a7a429938bd7f0c8d068
Anuradha Oruganti <anuradha.oruganti@ttb.gov>
2025-09-20 10:12:57 -0500

    fixed err

--- src/app/mapbox.config.ts
+++ src/app/mapbox.config.ts
@@ -1,2 +1,1 @@
-// Add your Mapbox API key here
-export const MAPBOX_API_KEY = pk.eyJ1IjoiY2hpbm5hc3JpbnUiLCJhIjoiY21mcWozY21tMGx5bzJscTFtYTkyOGN5NCJ9.v1Qyvyl6MHvf8Vn-sd6c-w;
+export const MAPBOX_API_KEY = 'pk.eyJ1IjoiY2hpbm5hc3JpbnUiLCJhIjoiY21mcWozY21tMGx5bzJscTFtYTkyOGN5NCJ9.v1Qyvyl6MHvf8Vn-sd6c-w';
